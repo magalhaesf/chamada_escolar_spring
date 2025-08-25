38dfff2da26f45560a2b790aba27b4ddc55cbff6
magalhaesf <fernanda.fatima.magalhaes@escola.pr.gov.br>
2025-08-25 11:49:44 -0300

    alterações

--- src/main/java/com/magalhaes/recuperacao/modelo/Disciplina.java
+++ src/main/java/com/magalhaes/recuperacao/modelo/Disciplina.java
@@ -1,9 +1,11 @@
 package com.magalhaes.recuperacao.modelo;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+@Entity
 public class Disciplina {
     
     @Id
