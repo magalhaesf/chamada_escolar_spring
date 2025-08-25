3c4d6763cd641c8406bd68a00778d13ef7a2e140
magalhaesf <fernanda.fatima.magalhaes@escola.pr.gov.br>
2025-08-25 15:20:34 -0300

    crud pronto

--- src/main/java/com/magalhaes/recuperacao/controller/DisciplinaController.java
+++ src/main/java/com/magalhaes/recuperacao/controller/DisciplinaController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -54,6 +55,33 @@ public class DisciplinaController {
         return ResponseEntity.noContent().build();
     }
 
-    
+    @GetMapping(value = "/buscar/{nome}")
+    public ResponseEntity<Disciplina> buscarPorNome(@PathVariable String nome){
+        Disciplina disciplina = disciplinaRepository.findByNome(nome);
+        if (disciplina == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(disciplina);
+    }
+
+    @SuppressWarnings("unchecked")
+    @PutMapping("/{id}")
+    public ResponseEntity<Disciplina> atualizar(@PathVariable Long id, RequestBody DisciplinaDto){
+         Optional<Disciplina> disciplinaOptional = disciplinaRepository.findById(id);
+
+        if (!disciplinaOptional.isPresent()){
+            return ResponseEntity.notFound().build();
+        }
+
+        Disciplina disciplina = disciplinaOptional.get();
+        DisciplinaDto dto = null;
+        disciplina.setNome(dto.getNome());
+
+        disciplinaRepository.save(disciplina);
+        return ResponseEntity.ok(disciplina);
+
+    }
+       
+        
 }
 
--- src/main/java/com/magalhaes/recuperacao/repository/DisciplinaRepository.java
+++ src/main/java/com/magalhaes/recuperacao/repository/DisciplinaRepository.java
@@ -1,5 +1,7 @@
 package com.magalhaes.recuperacao.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import com.magalhaes.recuperacao.modelo.Disciplina;
@@ -8,5 +10,8 @@ public interface DisciplinaRepository extends JpaRepository<Disciplina, Long>{
     
     @Query(value="select * from Disciplina where nome like ?", nativeQuery = true)
     public Disciplina findByNome(String nome);
+
+     @Query(value="select * from Disciplina where id like ?", nativeQuery = true)
+    public Optional findById(Long id);
 
 }
